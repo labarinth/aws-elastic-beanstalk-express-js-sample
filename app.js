8f412483cf2a0387c5b52323f5bcf51939251885
labarinth <charnet1@icloud.com>
2023-08-15 16:16:03 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Testing new sentence'));
+app.get('/', (req, res) => res.send('Testing another new sentence here again'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
